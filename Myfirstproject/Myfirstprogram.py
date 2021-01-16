c0f9e598b1bc80455c02382510790e5246a614a3
khizarsiddiqui882 <king_rank90@yahoo.com>
2021-01-16 15:59:35 +0500

    My First Python Project String Slicing

---
+++ Myfirstproject/Myfirstprogram.py
@@ -0,0 +1,18 @@
+a = ' Keyword \n '
+b = ' hazel \n '
+c = ' accomplished \n '
+d = ' analyze \n '
+e = ' rays \n '
+f = ' accordant \n '
+g = ' Sack \n '
+h = ' going \n '
+i = ' end \n '
+j = ' straightforward \n '
+k = ' with \n '
+l = ' unique \n '
+m = ' instrument \n '
+n = ' psychotherapist \n '
+
+print("The Words Used To Create Name Are:")
+print(a + ' ' + b + ' ' + c + ' ' + d + ' ' + e + ' ' + f + ' ' + g + ' ' + h + ' ' + i + ' ' + j + ' ' + k + ' ' + l + ' ' + m + ' ' + n)
+print("The Result Is:" + ' ' + a[1] + b[1] + c[8] + d[6] + e[2] + f[5] + ' ' + g[1] + h[3] + i[3] + j[15] + k[2] + l[4] + m[6] + n[13])
